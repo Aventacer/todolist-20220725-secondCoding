8c983eb4f9fdd8982875950755e2d053cdd3da9e
JAEWON-A46X\jaewon <jaewun1@gmail.com>
2022-07-27 21:41:48 +0900

    2022 07 27 importance list, complere, importance, update, delete

---
+++ src/main/java/com/jaewon/todolist/web/controller/api/TodoController.java
@@ -0,0 +1,121 @@
+package com.jaewon.todolist.web.controller.api;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jaewon.todolist.service.todo.TodoService;
+import com.jaewon.todolist.web.dto.CMRespDto;
+import com.jaewon.todolist.web.dto.todo.CreateTodoReqDto;
+import com.jaewon.todolist.web.dto.todo.TodoListRespDto;
+import com.jaewon.todolist.web.dto.todo.UpdateTodoReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/todolist")
+@RequiredArgsConstructor
+public class TodoController {
+	
+	private final TodoService todoService;
+	
+	@GetMapping("/list")
+	public ResponseEntity<?> getTodoList(@RequestParam int page, @RequestParam int contentCount){
+		List<TodoListRespDto> list = null;
+		
+		try {
+			list = todoService.getTodoList(page, contentCount);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(new CMRespDto<>(-1, page + " page list loading fail", list));
+		}
+		return ResponseEntity.ok().body(new CMRespDto<>(1, page + " page list loading success", list));
+	}
+	
+	@GetMapping("/list/importance")
+	public ResponseEntity<?> getImportanceTodoList(@RequestParam int page, @RequestParam int contentCount) {
+		List<TodoListRespDto> list = null;
+		
+		try {
+			list = todoService.getImportanceTodoList(page, contentCount);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(new CMRespDto<>(-1, "importance loading failed", list));
+		}
+		
+		return ResponseEntity.ok().body(new CMRespDto<>(1, "importance loading success", list));
+	}
+	
+	@PostMapping("/todo")
+	public ResponseEntity<?> addTodo(@RequestBody CreateTodoReqDto createTodoReqDto){
+		try {
+			if(!todoService.createTodo(createTodoReqDto)) {
+				throw new RuntimeException("DataBase Error!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(new CMRespDto<>(-1, "adding todo failed", createTodoReqDto));
+		}
+		
+		return ResponseEntity.ok().body(new CMRespDto<>(1, "success", createTodoReqDto));
+	}
+	
+	@PutMapping("/complete/todo/{todoCode}")
+	public ResponseEntity<?> setCompleteTodo(@PathVariable int todoCode) {
+		boolean status = false;
+		try {
+			status = todoService.updateTodoComplete(todoCode);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(new CMRespDto<>(-1, "failed", status));		
+		}
+		return ResponseEntity.ok().body(new CMRespDto<>(1, "success", status));		
+	}
+
+	@PutMapping("/importance/todo/{todoCode}")
+	public ResponseEntity<?> setImportanceTodo(@PathVariable int todoCode) {
+		boolean status = false;
+		try {
+			status = todoService.updateTodoImportance(todoCode);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(new CMRespDto<>(-1, "failed", status));		
+		}
+		return ResponseEntity.ok().body(new CMRespDto<>(1, "success", status));		
+	}
+	
+	@PutMapping("/todo/{todoCode}")
+	public ResponseEntity<?> setTodo(@PathVariable int todoCode, @RequestBody UpdateTodoReqDto updateTodoReqDto) {
+		boolean status = false;
+		try {
+			updateTodoReqDto.setTodoCode(todoCode);
+			status = todoService.updateTodo(updateTodoReqDto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(new CMRespDto<>(-1, "failed", status));		
+		}
+		return ResponseEntity.ok().body(new CMRespDto<>(1, "success", status));		
+	}
+	
+	@DeleteMapping("/todo/{todoCode}")
+	public ResponseEntity<?> removeTodo(@PathVariable int todoCode) {
+		boolean status = false;
+		try {
+			status = todoService.removeTodo(todoCode);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(new CMRespDto<>(-1, "failed", status));		
+		}
+		return ResponseEntity.ok().body(new CMRespDto<>(1, "success", status));
+	}
+}
